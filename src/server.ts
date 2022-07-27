42a69d78e35394a6bff374542161d2912cc0802f
Daniel Cunha <danielcc.jp@gmail.com>
2022-07-26 21:59:18 -0300

    fix dev script

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World!' });
+});
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.send({ name });
 });
 
 app.listen(3333, () => console.log('Server started on port 3333'));
