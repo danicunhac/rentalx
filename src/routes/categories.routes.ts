847b19d8ca952820463c721b800957440d243254
Daniel Cunha <danielcc.jp@gmail.com>
2022-08-20 11:33:49 -0300

    feat: category model with id

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const caterogies = [];
+const caterogies: Category[] = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   caterogies.push(category);
 
