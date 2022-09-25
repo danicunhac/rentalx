3289334e51ae0770a5d3b5121f7721b8bca8493e
Daniel Costa <danielcc.jp@gmail.com>
2022-09-25 11:22:46 -0300

    fix: remove wrong import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { response, Router } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
