d45ef0f6e6018066d065b0b01ded49fdd27ef5a9
Daniel Costa <danielcc.jp@gmail.com>
2022-09-25 11:21:55 -0300

    chore: setup multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.get('/', (req, res) =>
   listCategoriesController.handle(req, res)
@@ -12,5 +17,11 @@ categoriesRoutes.get('/', (req, res) =>
 categoriesRoutes.post('/', (req, res) =>
   createCategoryController.handle(req, res)
 );
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  const { file } = req;
+  console.log(file);
+  res.send();
+});
 
 export { categoriesRoutes };
